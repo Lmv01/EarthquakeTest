31bc74b9efacbceaa7fdb5379d0f06367b40d84b
Lmv01 <loganmvargas@gmail.com>
2024-01-31 10:32:23 -0800

    Initial commit

---
+++ app/src/main/java/com/example/earthquaketest/EarthquakeListActivity.kt
@@ -0,0 +1,13 @@
+package com.example.earthquaketest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_earthquake_list)
+
+
+    }
+}
